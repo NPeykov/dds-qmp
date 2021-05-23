fb1f8bb785fec1d80a1f3a04fd6dac376bd586eb
Peykovich <peykovn@gmail.com>
2021-05-23 01:57:46 -0300

    agrego exceptions

---
+++ src/main/java/Exceptions/ColorPrimarioNullException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ColorPrimarioNullException extends RuntimeException{
+  public ColorPrimarioNullException(String errorMessage){
+    super(errorMessage);
+  }
+}
---
+++ src/main/java/Exceptions/TipoPrendaNullException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class TipoPrendaNullException extends RuntimeException{
+  public TipoPrendaNullException(String errorMessage){
+    super(errorMessage);
+  }
+}
