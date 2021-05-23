5edd1681360b07f959c078af5b594725f26e9040
Peykovich <peykovn@gmail.com>
2021-05-23 02:00:12 -0300

    reinicio tests

--- src/test/java/model/PrendaTests.java
+++ src/test/java/model/PrendaTests.java
@@ -5,9 +5,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrendaTests {
 
-  @Test
-  void pruebaSetteoEntorno(){
-    Prenda nuevaPrenda = new Prenda();
-    assertEquals(2, nuevaPrenda.devuelve2());
-  }
 }
