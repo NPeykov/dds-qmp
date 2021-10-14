cba908b5cc44582fd8a88b0ea200660159c9f5fe
peykov <peykovn@gmail.com>
2021-10-14 14:09:01 -0300

    agrego y pruebo dependencia handlebars

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,14 +1,21 @@
 package server;
 
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Server {
 
   public static void main(String[] args) {
+    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
     Spark.port(9000);
     Spark.get("/", (request, response) ->
-        "<h2>que me pongo?</h2>" +
-        "<p>q8-)</p>");
+        new ModelAndView(
+            request.queryParamOrDefault("ropa", "Ropa"),
+            "saludo.html.hbs"
+        ),
+        engine
+    );
   }
 
 }
